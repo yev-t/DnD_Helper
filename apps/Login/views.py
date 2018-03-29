8d0383942ed363cff879c6e39730efb08ca7b8fc
Yevgeniy Tyurmin <yev.tyurmin@gmail.com>
2018-03-29 12:01:52 -0400

    initial commit

---
+++ apps/Login/views.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, redirect
+
+# Create your views here.
+
+def index(request):
+	pass  #login page
+
+def registration(request):
+	pass
+
+def register_user(request):
+	pass
+
+def login_user(request):
+	pass
